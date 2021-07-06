8a7410282c7812e9514c4052b1efcc132630e427
9comindia <67412987+9comindia@users.noreply.github.com>
2021-07-07 01:04:52 +0530

    Topbar removed

--- app/src/main/java/com/srilakshmichennakeshavahighschoolmidthur/MainActivity.java
+++ app/src/main/java/com/srilakshmichennakeshavahighschoolmidthur/MainActivity.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
+
+public class MainActivity extends Activity {
 
     private WebView webView;
 
@@ -18,14 +26,32 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // START -- copy from here
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // END -- copy till here
+
         setContentView(R.layout.activity_main);
 
         CustomWebViewClient client = new CustomWebViewClient(this);
         webView = findViewById(R.id.webView);
         webView.setWebViewClient(client);
+/*
+        webView.getSettings().setAllowContentAccess(true);
+        webView.getSettings().setAllowFileAccess(true);
 
+        webView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                webView.loadUrl("file:///error.html");
+
+            }
+        });
+
+ */
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://slc-school.com/post");
+
     }
 
     @Override
@@ -49,13 +75,30 @@ class CustomWebViewClient extends WebViewClient{
     //API level less than 24
     @Override
     public boolean shouldOverrideUrlLoading(WebView webView, String url){
+/*
+        if (url != null && url.contains("slc-school.com")) {
+            //do what you want here
+            return false;
+        } else {
+            return true;
+        }
+
+ */
         return false;
     }
 
+
+/*
     //API level greater than= 24
     @Override
     public boolean shouldOverrideUrlLoading(WebView webView, WebResourceRequest request){
         return false;
     }
+*/
+
+
+
+
+
 
 }
